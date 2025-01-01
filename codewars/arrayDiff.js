a70620a72f1fb62e4625745a2887b3ec4449a3ed
zhfrann <muhzhafranilham13@gmail.com>
2025-01-02 03:51:17 +0700

    solve array diff problem

---
+++ codewars/arrayDiff.js
@@ -0,0 +1,23 @@
+//kata : https://www.codewars.com/kata/523f5d21c841566fde000009/javascript
+// Your goal in this kata is to implement a difference function, 
+// which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+[1, 2, 3, 4].includes()
+
+function arrayDiff(a, b) {
+    return a.filter((value) => !b.includes(value));
+}
+
+console.log(arrayDiff([1, 2], [1]))
+console.log(arrayDiff([1, 2, 2], [1]));
+console.log(arrayDiff([1, 2, 2], [2]));
+console.log(arrayDiff([1, 2, 2], []));
+console.log(arrayDiff([1, 2, 2], []));
+console.log(arrayDiff([1, 2, 3], [1, 2]));
