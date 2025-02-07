780a48cf86e4454edc3cdd097d3380d57c91208d
zhfrann <muhzhafranilham13@gmail.com>
2025-02-07 19:59:50 +0700

    solve binary tree inorder traversal problem

---
+++ leetcode/binaryTreeInorderTraversal.js
@@ -0,0 +1,54 @@
+// Given the root of a binary tree, return the inorder (left-root-right) traversal of its nodes' values.
+
+// Example 1:
+// Input: root = [1,null,2,3]
+// Output: [1,3,2]
+
+// Example 2:
+// Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+// Output: [4,2,6,5,7,1,3,9,8]
+
+// Example 3:
+// Input: root = []
+// Output: []
+
+// Example 4:
+// Input: root = [1]
+// Output: [1]
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+ 
+
+// Follow up: Recursive solution is trivial, could you do it iteratively?
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let stack = [], result = [];
+    
+    while (root !== null || stack.length > 0) {
+        if (root !== null) {
+            stack.push(root)
+            root = root.left;
+            continue;
+        }
+
+        root = stack.pop();
+        result.push(root.val);
+
+        root = root.right;
+    }
+    return result;
+};
